ec4e0bd849d29fadab6870615fc774f71b725382
Leeeast <lidongliang_1990@126.com>
2017-09-26 10:12:06 +0800

    添加base
    创建base包

--- app/src/main/java/com/art/huakai/artshow/activity/MainActivity.java
+++ app/src/main/java/com/art/huakai/artshow/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.art.huakai.artshow;
+package com.art.huakai.artshow.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.art.huakai.artshow.R;
 
 public class MainActivity extends AppCompatActivity {
 
