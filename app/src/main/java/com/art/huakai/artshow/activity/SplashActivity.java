7d8ea51ee56a74994071312a7dee714805db1666
Leeeast <lidongliang_1990@126.com>
2017-09-27 15:39:54 +0800

    添加启动页

---
+++ app/src/main/java/com/art/huakai/artshow/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.art.huakai.artshow.activity;
+
+import com.art.huakai.artshow.R;
+import com.art.huakai.artshow.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    public void immerseStatusBar() {
+
+    }
+
+    @Override
+    public int getLayoutID() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setView() {
+
+    }
+}
