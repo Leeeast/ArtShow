c1267a7ea0f44dd4f42a9c04c1e258a26fdb57df
lidongliang <dongfang369>
2017-10-16 04:07:46 +0800

    基本资料添加

--- app/src/main/java/com/art/huakai/artshow/activity/BaseDataActivity.java
+++ app/src/main/java/com/art/huakai/artshow/activity/BaseDataActivity.java
@@ -2,6 +2,7 @@ package com.art.huakai.artshow.activity;
 
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.art.huakai.artshow.R;
 import com.art.huakai.artshow.base.BaseActivity;
@@ -11,7 +12,7 @@ import butterknife.BindView;
 import butterknife.OnClick;
 
 /**
- * 简历Activity
+ * 基本资料Activity
  * Created by lidongliang on 2017/10/14.
  */
 
@@ -19,6 +20,8 @@ public class BaseDataActivity extends BaseActivity {
 
     @BindView(R.id.tv_title)
     TextView tvTitle;
+    @BindView(R.id.tv_subtitle)
+    TextView tvSubtitle;
 
     @Override
     public void immerseStatusBar() {
@@ -37,8 +40,8 @@ public class BaseDataActivity extends BaseActivity {
     @Override
     public void initView() {
         tvTitle.setVisibility(View.VISIBLE);
-        tvTitle.setText(R.string.resume_my);
-
+        tvTitle.setText(R.string.resume_base_data);
+        tvSubtitle.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -54,5 +57,12 @@ public class BaseDataActivity extends BaseActivity {
         finish();
     }
 
+    /**
+     * 确认信息
+     */
+    @OnClick(R.id.tv_subtitle)
+    public void commitInfo() {
+        Toast.makeText(this, "确认信息", Toast.LENGTH_SHORT).show();
+    }
 
 }
